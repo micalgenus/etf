ff7585001b09c15e49c8d452aa13bd193e3362d4
GyeongSu Han <micalgenus@gmail.com>
2019-11-21 00:32:54 +0900

    Init spring boot (#2)

---
+++ backend/src/test/java/com/micalgenus/etf/EtfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micalgenus.etf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
