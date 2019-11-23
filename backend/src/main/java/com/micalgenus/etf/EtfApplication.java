91433f542b877fdb530b80c0e1959cde5d59c79a
GyeongSu Han <micalgenus@gmail.com>
2019-11-23 21:33:49 +0900

    Apply lint in backend with checkstyle (#13)

--- backend/src/main/java/com/micalgenus/etf/EtfApplication.java
+++ backend/src/main/java/com/micalgenus/etf/EtfApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EtfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EtfApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EtfApplication.class, args);
+  }
 
 }
