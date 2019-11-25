2b90876cc5024b63c38a48a7c0c1248755e546cb
GyeongSu Han <micalgenus@gmail.com>
2019-11-25 22:15:18 +0900

    Run development mode of docker in backend (#19)
    * Update watching mode in gradle
    
    * Apply custom watchman for continuous mode of gradle
    
    * Update check time 2s => 4s

---
+++ backend/tools/watchman.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+./gradlew build
+
+echo 'Watching file in src/**/*'
+
+while :;
+do
+  if [ ! -z $(find ./src -mtime -0.00004) ]; then
+    ./gradlew build;
+  fi;
+  sleep 1;
+done
