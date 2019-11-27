3901c18804bdf7179ec6dc05428e52609fb3c969
GyeongSu Han <micalgenus@gmail.com>
2019-11-27 21:37:36 +0900

    Add spring boot starter security (#20)

--- backend/src/main/java/com/micalgenus/etf/Application.java
+++ backend/src/main/java/com/micalgenus/etf/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EtfApplication {
+public class Application {
   /**
    * Initializing Spring boot application for running period.
    */
   public static void main(String[] args) {
-    new SpringApplicationBuilder(EtfApplication.class)
+    new SpringApplicationBuilder(Application.class)
       .properties("spring.config.location=classpath:/application.yml")
       .run(args);
   }
