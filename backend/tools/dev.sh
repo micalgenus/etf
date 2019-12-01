c44eba161af6af2f66c11807ddb581f4685303f7
GyeongSu Han <micalgenus@gmail.com>
2019-12-01 14:51:45 +0900

    Update watchman for backend (#28)

--- backend/tools/dev.sh
+++ backend/tools/dev.sh
@@ -12,4 +12,4 @@ echo "123456
 yes
 " | keytool -genkey -alias bns-ssl -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 36504 1>/dev/null
 
-./gradlew bootRun -Pdev -t
+./gradlew bootRun -Pdev -t -x test -x checkstyleMain
--- backend/tools/watchman.sh
+++ backend/tools/watchman.sh
@@ -4,10 +4,12 @@
 
 echo 'Watching file in src/**/*'
 
+(
 while :;
 do
   if [ ! -z $(find ./src -mtime -0.00004) ]; then
-    ./gradlew build -x test ;
+    ./gradlew build -x test -x checkstyleMain ;
   fi;
   sleep 1;
 done
+) & ./tools/dev.sh
