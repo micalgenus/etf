9f43e0e4635ece0e81bb6f1ed7d98ae8c0e3f251
GyeongSu Han <micalgenus@gmail.com>
2020-03-30 22:27:05 +0900

    Apply redis configuration

---
+++ backend/src/main/java/com/blackcowmoo/etf/config/RedisConfig.java
@@ -0,0 +1,51 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+  @Value("${spring.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
+  // @Autowired
+  // private final RedisProperties redisProperties;
+
+  // lettuce
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(redisHost);
+    redisStandaloneConfiguration.setPort(redisPort);
+    // 패스워드 있으면 설정
+    // redisStandaloneConfiguration.setPassword(redisPwd);
+    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+    return lettuceConnectionFactory;
+
+    // return new LettuceConnectionFactory(redisProperties.getHost(),
+    // redisProperties.getPort());
+  }
+
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate() {
+    RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    return redisTemplate;
+  }
+}
---
+++ backend/src/test/java/com/blackcowmoo/etf/config/RedisTest.java
@@ -0,0 +1,35 @@
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+  @Autowired
+  RedisTemplate<String, Object> redisTemplate;
+
+  @Test
+  public void redisBasicFunction_guide() throws JSONException {
+    // Given
+    String key = "iamkey";
+    String value = "iamvalue";
+
+    // redis에 set
+    redisTemplate.opsForValue().set(key, value);
+
+    // redis에서 get
+    RedisVO redisVO = (RedisVO) redisTemplate.opsForValue().get(key);
+
+    // redis에서 data delete
+    redisTemplate.delete(key);
+
+    // redis에 해당 key를 가지고 있는지 확인
+    if (!redisTemplate.hasKey("999")) {
+      System.out.println("key 미존재");
+    }
+  }
+}
